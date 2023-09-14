1150760f027702c4f5e657c169551c8f9dc1b5f8
Pallas <vincent.bollot@gmail.com>
2023-09-14 15:31:46 +0200

    change main and add gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 5.16.2"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1" # AWS region
+  region = "eu-west-3" # AWS region
 }
 
 # provider "aws" {
@@ -29,14 +30,14 @@ resource "aws_vpc" "kubernetes_vpc" {
 resource "aws_subnet" "kubernetes_subnet" {
   vpc_id                  = aws_vpc.kubernetes_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a" # Change this to your desired availability zone
-  map_public_ip_on_launch = true #public ip au lancement 
+  availability_zone       = "eu-west-3c" # Change this to your desired availability zone
+  map_public_ip_on_launch = true         #public ip au lancement 
   tags = {
     Name = "kubernetes"
   }
 }
 
-#internet gateway link of item VPC aws_vpc (passerelle)
+#Internet gateway link of item VPC aws_vpc (passerelle)
 resource "aws_internet_gateway" "kubernetes_igw" {
   vpc_id = aws_vpc.kubernetes_vpc.id
   tags = {
@@ -44,6 +45,7 @@ resource "aws_internet_gateway" "kubernetes_igw" {
   }
 }
 
+#Table route for VPC
 resource "aws_route_table" "kubernetes_route_table" {
   vpc_id = aws_vpc.kubernetes_vpc.id
   route {
@@ -55,81 +57,87 @@ resource "aws_route_table" "kubernetes_route_table" {
   }
 }
 
+#Sécurity group
 resource "aws_security_group" "kubernetes_security_group" {
-  name        = "kubernetes"
-  description = "Kubernetes security group"
-  vpc_id      = aws_vpc.kubernetes_vpc.id
+  name        = "kubernetes"                # Nom du groupe de sécurité
+  description = "Kubernetes security group" # Description du groupe de sécurité
+  vpc_id      = aws_vpc.kubernetes_vpc.id   # ID de la VPC à associer au groupe de sécurité
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16", "10.200.0.0/16", "0.0.0.0/0"]
+    from_port   = 0                                # Port de début de la plage de ports autorisée
+    to_port     = 65535                            # Port de fin de la plage de ports autorisée
+    protocol    = "all"                            # Protocole autorisé (TCP dans ce cas)
+    cidr_blocks = ["10.0.0.0/16", "10.200.0.0/16"] # Plages IP autorisées
   }
 
   ingress {
-    from_port   = 22
+    from_port   = 22 # Autoriser le trafic SSH sur le port 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Autoriser depuis n'importe quelle adresse IP
   }
 
   ingress {
-    from_port   = 6443
+    from_port   = 6443 # Autoriser le trafic sur le port 6443 (Kubernetes)
     to_port     = 6443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 443
+    from_port   = 443 # Autoriser le trafic HTTPS sur le port 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = -1
+    from_port   = -1 # Autoriser le trafic ICMP (ping)
     to_port     = -1
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_lb" "kubernetes_lb" {
-  name               = "kubernetes"
-  internal           = false
-  load_balancer_type = "network"
-  subnets            = [aws_subnet.kubernetes_subnet.id]
-}
 
-resource "aws_lb_target_group" "kubernetes_target_group" {
-  name     = "kubernetes"
-  port     = 6443
-  protocol = "TCP"
-  vpc_id   = aws_vpc.kubernetes_vpc.id
-  target_type = "ip"
-}
+#load balancer
+# resource "aws_lb" "kubernetes_lb" {
+#   name               = "kubernetes"
+#   internal           = false
+#   load_balancer_type = "network"
+#   subnets            = [aws_subnet.kubernetes_subnet.id]
+# }
 
-resource "aws_lb_listener" "kubernetes_listener" {
-  load_balancer_arn = aws_lb.kubernetes_lb.arn
-  port              = 443
-  protocol          = "TCP"
+# #load balancer target group
+# resource "aws_lb_target_group" "kubernetes_target_group" {
+#   name     = "kubernetes"
+#   port     = 6443
+#   protocol = "TCP"
+#   vpc_id   = aws_vpc.kubernetes_vpc.id
+#   target_type = "ip"
+# }
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.kubernetes_target_group.arn
-  }
-}
+#load balancer listener
+# resource "aws_lb_listener" "kubernetes_listener" {
+#   load_balancer_arn = aws_lb.kubernetes_lb.arn
+#   port              = 443
+#   protocol          = "TCP"
 
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.kubernetes_target_group.arn
+#   }
+# }
+
+#Instance Controllers
 resource "aws_instance" "kubernetes_controllers" {
-  count = 3
-  ami           = "ami-xxxxxxxxxxxxxx" # Replace with the desired AMI ID
-  instance_type = "t3.micro"
+  count         = 3
+  ami           = "ami-05b5a865c3579bbc4" # Replace with the desired AMI ID
+  instance_type = "t2.micro"
   key_name      = "kubernetes"
   subnet_id     = aws_subnet.kubernetes_subnet.id
   private_ip    = "10.0.1.1${count.index}"
-  user_data = <<-EOF
+  user_data     = <<-EOF
               name=controller-${count.index}
               EOF
   root_block_device {
@@ -138,16 +146,21 @@ resource "aws_instance" "kubernetes_controllers" {
   tags = {
     Name = "controller-${count.index}"
   }
+
 }
 
+output "kubernetes_public_address_ip_controllers" {
+  value = [for instance in aws_instance.kubernetes_controllers : instance.public_ip]
+}
+#Instances Workers
 resource "aws_instance" "kubernetes_workers" {
-  count = 3
-  ami           = "ami-xxxxxxxxxxxxxx" # Replace with the desired AMI ID
-  instance_type = "t3.micro"
+  count         = 3
+  ami           = "ami-05b5a865c3579bbc4" # Replace with the desired AMI ID
+  instance_type = "t2.micro"
   key_name      = "kubernetes"
   subnet_id     = aws_subnet.kubernetes_subnet.id
   private_ip    = "10.0.1.2${count.index}"
-  user_data = <<-EOF
+  user_data     = <<-EOF
               name=worker-${count.index}|pod-cidr=10.200.${count.index}.0/24
               EOF
   root_block_device {
@@ -156,8 +169,15 @@ resource "aws_instance" "kubernetes_workers" {
   tags = {
     Name = "worker-${count.index}"
   }
+
 }
 
-output "kubernetes_public_address" {
-  value = aws_lb.kubernetes_lb.dns_name
+output "kubernetes_public_address_ip_workers" {
+  value = [for instance in aws_instance.kubernetes_workers : instance.public_ip]
 }
+
+
+
+# output "kubernetes_public_address" {
+#   value = aws_lb.kubernetes_lb.dns_name
+# }
