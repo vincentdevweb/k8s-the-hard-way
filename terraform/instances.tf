d3d13f5357f9fbf107b6676bc425157963a473c3
Pallas <vincent.bollot@gmail.com>
2023-09-18 15:13:26 +0200

    lb change to work --- Work in progress ---

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -5,11 +5,11 @@ resource "aws_key_pair" "kubernetes_key" {
 
 #Instance Controllers
 resource "aws_instance" "kubernetes_controllers" {
-  count         = 1
-  ami           = "ami-05b5a865c3579bbc4"
-  instance_type = "t3.micro"
-  key_name      = aws_key_pair.kubernetes_key.key_name
-  subnet_id     = aws_subnet.kubernetes_subnet.id
+  count                  = 1
+  ami                    = "ami-05b5a865c3579bbc4"
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.kubernetes_key.key_name
+  subnet_id              = aws_subnet.kubernetes_subnet.id
   vpc_security_group_ids = [aws_security_group.kubernetes_security_group.id]
   private_ip             = "10.0.1.1${count.index}"
   user_data              = <<-EOF
--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,22 +1,35 @@
-
-#load balancer == OPTIMISATION
+# # Creation du load balancer pour le cluster
 # resource "aws_lb" "kubernetes_lb" {
 #   name               = "kubernetes"
 #   internal           = false
 #   load_balancer_type = "network"
 #   subnets            = [aws_subnet.kubernetes_subnet.id]
+#   tags = {
+#     name = "lb_kubernetes"
+#   }
 # }
 
-# #load balancer target group
+# #Load balancer pour un groupe cible crée
 # resource "aws_lb_target_group" "kubernetes_target_group" {
-#   name     = "kubernetes"
-#   port     = 6443
-#   protocol = "TCP"
-#   vpc_id   = aws_vpc.kubernetes_vpc.id
+#   name        = "kubernetes"
+#   port        = 6443
+#   protocol    = "TCP"
+#   vpc_id      = aws_vpc.kubernetes_vpc.id
 #   target_type = "ip"
+#   tags = {
+#     name = "lb_kubernetes_target"
+#   }
 # }
 
-#load balancer listener
+# # Création d'une cible pour le groupe
+# resource "aws_lb_target_group_attachment" "kubernetes" {
+#   count            = 1 # kubernetes_controllers_instance_count
+#   target_group_arn = aws_lb_target_group.kubernetes_target_group.arn
+#   target_id        = aws_instance.kubernetes_controllers[count.index].private_ip
+#   port             = 6443
+# }
+
+# # Load balancer association avec le groupe cible
 # resource "aws_lb_listener" "kubernetes_listener" {
 #   load_balancer_arn = aws_lb.kubernetes_lb.arn
 #   port              = 443
@@ -30,4 +43,9 @@
 
 # output "kubernetes_public_address" {
 #   value = aws_lb.kubernetes_lb.dns_name
+# }
+
+# # Récupération de l'adresse IP du load balancer
+# data "aws_lb" "kubernetes" {
+#   arn = aws_lb.kubernetes_lb.arn
 # }
